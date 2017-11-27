8b3a37dfdd40bb72610d1c06bf65b3414cf34bfe
Abdul wakeel <itsprogrammer@gmail.com>
2017-11-27 14:24:02 +0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 ﻿export const environment = {
   production: true,
-  metaDataAPI: 'http://sagssystem/ex_sqlSvc/REST/Service.svc/Entity/DL_CMSView/',
-  dataAPI: 'http://sagssystem/ex_sqlSvc/REST/Service.svc/Entity/',
-  actionsAPI: 'http://sagssystem/ex_sqlSvc/REST/Service.svc/Entity/DL_Action',
-  dataAPIProcedure: 'http://sagssystem/ex_sqlSvc/REST/Service.svc/Procedure/',
-  menuIconURL: '/EX_Resources/gif/16X16/',
+  metaDataAPI: 'http://',
+  dataAPI: 'http://',
+  actionsAPI: 'http:',
+  dataAPIProcedure: '',
+  menuIconURL: '',
   local: false,
   cache:false,
 };
